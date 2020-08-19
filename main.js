e32b9d6ace93b198f02f2c8800caabf74ea8d5f4
Maciej Wasiak <mwasiak154@gmail.com>
2020-08-19 14:39:26 +0200

    local storage

--- main.js
+++ main.js
@@ -3,18 +3,28 @@ const deathCounter = document.querySelector(".death-counter");
 const victoryCounter = document.querySelector(".victory-counter");
 const result = document.querySelector(".result");
 let rng;
-let deathQuantity = 0;
-let victoryQuantity = 0;
+
+if (localStorage.getItem('deathCounter') == null) {
+  localStorage.setItem('deathCounter', 0)
+}
+if (localStorage.getItem('victoryCounter') == null) {
+  localStorage.setItem('victoryCounter', 0)
+}
+
+let deathQuantity = parseFloat(localStorage.getItem('deathCounter'));
+let victoryQuantity = parseFloat(localStorage.getItem('victoryCounter'));
 
 playButton.addEventListener("click", function (event) {
   rng = Math.floor(Math.random() * 100 + 1);
   if (rng < 5) {
     result.innerHTML = "<p class='victory'>VICTORY ACHIEVED</p>";
     victoryQuantity += 1;
+    localStorage.setItem('victoryCounter', victoryQuantity)
     victoryCounter.innerHTML = "Victory Count: " + victoryQuantity;
   } else {
     result.innerHTML = "<p class='death'>YOU DIED</p>";
     deathQuantity += 1;
+    localStorage.setItem('deathCounter', deathQuantity)
     deathCounter.innerHTML = "Death Count: " + deathQuantity;
   }
 });
