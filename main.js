c05a4c37e28e92e45c96dcc447373fd4b502c6b1
Maciej Wasiak <mwasiak154@gmail.com>
2020-08-19 16:36:32 +0200

    layout

--- main.js
+++ main.js
@@ -6,25 +6,31 @@ let rng;
 
 if (localStorage.getItem('deathCounter') == null) {
   localStorage.setItem('deathCounter', 0)
+} else {
+  deathCounter.innerHTML = "Deaths: " + parseFloat(localStorage.getItem('deathCounter'));
 }
 if (localStorage.getItem('victoryCounter') == null) {
   localStorage.setItem('victoryCounter', 0)
+} else {
+  victoryCounter.innerHTML = "Victories: " + parseFloat(localStorage.getItem('victoryCounter'));
 }
 
 let deathQuantity = parseFloat(localStorage.getItem('deathCounter'));
 let victoryQuantity = parseFloat(localStorage.getItem('victoryCounter'));
 
 playButton.addEventListener("click", function (event) {
+  document.querySelector('.result').style.background = 
+    'linear-gradient(transparent,black, rgba(0, 0, 0, 0.9), black, transparent)';
   rng = Math.floor(Math.random() * 100 + 1);
   if (rng < 5) {
     result.innerHTML = "<p class='victory'>VICTORY ACHIEVED</p>";
     victoryQuantity += 1;
     localStorage.setItem('victoryCounter', victoryQuantity)
-    victoryCounter.innerHTML = "Victory Count: " + victoryQuantity;
+    victoryCounter.innerHTML = "Victories: " + victoryQuantity;
   } else {
     result.innerHTML = "<p class='death'>YOU DIED</p>";
     deathQuantity += 1;
     localStorage.setItem('deathCounter', deathQuantity)
-    deathCounter.innerHTML = "Death Count: " + deathQuantity;
+    deathCounter.innerHTML = "Deaths: " + deathQuantity;
   }
 });
